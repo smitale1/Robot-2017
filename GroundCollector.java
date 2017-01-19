578d2b3330a0534d3818fe0d32bb1f74cf03cd77
Artiui <atharv.gawde@gmail.com>
2017-01-18 20:43:17 -0500

    Update GroundCollector.java

--- GroundCollector.java
+++ GroundCollector.java
@@ -1,5 +1,40 @@
 package org.usfirst.frc.team20.robot;
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.DoubleSolenoid;;
 
 public class GroundCollector {
+	CANTalon elevatorMotorFront;
+	CANTalon elevatorMotorBack;
 
+	public GroundCollector(){
+		elevatorMotorFront = new CANTalon(0);
+		elevatorMotorBack = new CANTalon(0);
+	}
+
+	public void intake(double speed) {
+		elevatorMotorFront.set(speed);
+		elevatorMotorBack.set(-speed);
+	}
+
+	public void outtake(double speed) {
+		elevatorMotorFront.set(-speed);
+		elevatorMotorBack.set(speed);
+	}
+	private DoubleSolenoid collectorPistons = new DoubleSolenoid(constants.COLLECTER_FIRST_SOLENOID_PORT,
+			constants.COLLECTER_SECOND_SOLENOID_PORT);
+
+	public void Collecters() {
+	}
+	public void retractCollectors() {
+		collectorPistons.set(DoubleSolenoid.Value.kReverse);
+	}
+
+	public void actuateCollectors() {
+		collectorPistons.set(DoubleSolenoid.Value.kForward);
+	}
+
+	public void neturalCollectors() {
+		collectorPistons.set(DoubleSolenoid.Value.kOff);
+	}
 }
+
